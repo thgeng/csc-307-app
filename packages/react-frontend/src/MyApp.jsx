9f2da2cad3ce464994430e67852779cc66e7c9ae
thgeng <thgeng@calpoly.edu>
2026-04-02 20:43:02 -0700

    Complete React first app with form submission

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,13 +1,9 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    { name: "Charlie", job: "Janitor" },
-    { name: "Mac", job: "Bouncer" },
-    { name: "Dee", job: "Aspiring actress" },
-    { name: "Dennis", job: "Bartender" },
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -17,12 +13,17 @@ function MyApp() {
     setCharacters(updated);
   }
 
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
   return (
     <div className="container">
       <Table
         characterData={characters}
         removeCharacter={removeOneCharacter}
       />
+      <Form handleSubmit={updateList} />
     </div>
   );
 }
