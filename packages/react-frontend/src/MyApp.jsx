fa627c9c2a788f20eaaac6158cc64b3ad8d4f7e3
thgeng <thgeng@calpoly.edu>
2026-04-02 20:20:14 -0700

    added remove section and delete botton.

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,17 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Table from "./Table";
 
 function MyApp() {
-  const characters = [
+  const [characters, setCharacters] = useState([
     { name: "Charlie", job: "Janitor" },
     { name: "Mac", job: "Bouncer" },
     { name: "Dee", job: "Aspiring actress" },
     { name: "Dennis", job: "Bartender" },
-  ];
+  ]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+
+    setCharacters(updated);
+  }
 
   return (
     <div className="container">
-      <Table characterData={characters} />
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
     </div>
   );
 }
--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -1,24 +1,28 @@
 import React from "react";
 
-// Header
 function TableHeader() {
   return (
     <thead>
       <tr>
         <th>Name</th>
         <th>Job</th>
+        <th>Remove</th>
       </tr>
     </thead>
   );
 }
 
-// Body
 function TableBody(props) {
   const rows = props.characterData.map((row, index) => {
     return (
       <tr key={index}>
         <td>{row.name}</td>
         <td>{row.job}</td>
+        <td>
+          <button onClick={() => props.removeCharacter(index)}>
+            Delete
+          </button>
+        </td>
       </tr>
     );
   });
@@ -26,12 +30,14 @@ function TableBody(props) {
   return <tbody>{rows}</tbody>;
 }
 
-// Main Table
 function Table(props) {
   return (
     <table>
       <TableHeader />
-      <TableBody characterData={props.characterData} />
+      <TableBody
+        characterData={props.characterData}
+        removeCharacter={props.removeCharacter}
+      />
     </table>
   );
 }
